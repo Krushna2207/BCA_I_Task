b0387d1d357cf3af4b3be2166ce7b889af3d9b79
Krushna2207 <145003867+Krushna2207@users.noreply.github.com>
2023-09-20 23:16:00 +0530

    Create touch file_name.js

---
+++ touch file_name.js
@@ -0,0 +1,8 @@
+$ gh pr create
+
+Creating pull request for feature-branch into main in ampinsk/test
+
+? Title My new pull request
+? Body [(e) to launch vim, enter to skip]
+? What's next? Submit
+https://github.com/owner/repo/pull/1
